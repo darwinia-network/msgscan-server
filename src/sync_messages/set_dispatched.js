3e8bd9bad2c62efdc7822f9446663cd4f62d5961
Aki Wu <wuminzhe@gmail.com>
2024-04-13 14:56:24 +0800

    rm useless comment

--- src/sync_messages/set_dispatched.js
+++ src/sync_messages/set_dispatched.js
@@ -28,29 +28,3 @@ async function setDispatched(messageFromChainId) {
 }
 
 export default setDispatched
-
-// messages = Message.where(from_chain_id: network.chain_id)
-//                   .where(status: %i[accepted root_ready])
-//
-// messages.each do |message|
-//   dispatched_log = Log.where(chain_id: message.to_chain_id)
-//                       .where(event_name: 'MessageDispatched')
-//                       .field_eq('msg_hash', message.msg_hash)
-//                       .first
-//   next if dispatched_log.nil?
-//
-//   message.dispatch_transaction_hash = dispatched_log.transaction_hash
-//   message.dispatch_block_number = dispatched_log.block_number
-//   message.dispatch_block_timestamp = Time.at(dispatched_log.timestamp)
-//   message.status = if dispatched_log.decoded['dispatch_result']
-//                      Message.statuses[:dispatch_success]
-//                    else
-//                      Message.statuses[:dispatch_failed]
-//                    end
-//
-//   # update proof
-//   transaction = Transaction.find_by_transaction_hash(dispatched_log.transaction_hash)
-//   message.proof = transaction.input[(-32 * 64)..].scan(/.{64}/).map { |item| "0x#{item}" }
-//
-//   message.save!
-// end
