1d580378415ae3b834931a14b5b33e3c8e4020b6
Aki Wu <wuminzhe@gmail.com>
2024-04-14 21:21:33 +0800

    fix table name

--- src/db/transaction.js
+++ src/db/transaction.js
@@ -3,7 +3,7 @@ import sql from './db.js'
 async function findTransactionByHash(chainId, transactionHash) {
   const result = await sql`
     SELECT *
-    FROM ponder_sync."Transaction"
+    FROM ponder_sync."transactions"
     WHERE "chainId" = ${chainId} and "hash" = ${transactionHash}
   `
   return result.length == 0 ? null : result[0]
